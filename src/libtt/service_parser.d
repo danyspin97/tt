5e185204bcb47f1a76ee095059c09d0d3563571a
Danilo Spinella <danyspin97@protonmail.com>
2019-12-14 15:51:42 +0100

    service_parser: Add new functions reading the ini
    Add new method to parse the ini file and a declare a IniFormat
    suited for the service files.

--- src/libtt/service_parser.d
+++ src/libtt/service_parser.d
@@ -2,6 +2,7 @@ module libtt.service_parser;
 
 import std.conv : to;
 import std.file;
+import std.string : toStringz ;
 
 import std.experimental.logger;
 
@@ -15,10 +16,11 @@ import libtt.bundle;
 import libtt.define;
 import libtt.dirs;
 
-alias RowParser = const int function(string key, string value, Service service);
+import libtt.section_parser;
 
+alias RowParser = const int function(string key, string value, ref Service service);
 
-int service_parser(bool system_mode = true) {
+int parse_all_services(bool system_mode = true) {
     immutable string[] dirs_to_read = [ dirs.service, dirs.admin_service ];
 
     string[] services;
@@ -31,6 +33,21 @@ int service_parser(bool system_mode = true) {
     return 0;
 }
 
+auto parse_services(string [] service_names, bool system_mode = true) {
+    immutable string[] dirs_to_read = [ dirs.service, dirs.admin_service ];
+
+    Service[] services;
+
+    foreach(dir; dirs_to_read) {
+        foreach(name; service_names) {
+            auto path = dir ~ "/" ~ name;
+            if (exists(path) && isFile(path)) {
+                services ~= parse_single_service(path);
+            }
+        }
+    }
+    return services;
+}
 
 extern (C) int service_dispatcher (IniDispatch* dispatch, void* service_ptr) {
     const RowParser[string] key_parse_dispatch = [
@@ -65,5 +82,45 @@ extern (C) int service_dispatcher (IniDispatch* dispatch, void* service_ptr) {
     }
 
     return 1;
+}
+
+IniFormat get_service_format() {
+    IniFormat f;
+
+    f.delimiter_symbol = IniDelimiters.INI_EQUALS;
+    f.case_sensitive = 1;
+    f.semicolon_marker = IniCommentMarker.INI_IGNORE;
+    f.hash_marker = IniCommentMarker.INI_IGNORE;
+    f.multiline_nodes = IniMultiline.INI_NO_MULTILINE;
+    f.section_paths = IniSectionPaths.INI_ABSOLUTE_ONLY;
+    f.no_single_quotes = 1;
+    f.no_double_quotes = 0;
+    f.no_spaces_in_names = 1;
+    f.implicit_is_not_empty = 1;
+
+    return f;
+}
+
+Service parse_single_service(string path) {
+    Service service;
+
+    uint ret = load_ini_path(
+                toStringz(path),
+                get_service_format(),
+                null,
+                &service_dispatcher,
+                &service);
+
+    if (ret == CONFINI_ERROR) {
+        criticalf("Something went wrong when parsing file %s", path);
+    } else if(ret == ConfiniInterruptNo.CONFINI_ENOMEM) {
+        criticalf("Could not alloc memory when reading file %s", path);
+    } else if (ret == ConfiniInterruptNo.CONFINI_ENOENT) {
+        logf("No file at: %s", path);
+    } else if (ret == ConfiniInterruptNo.CONFINI_EIO) {
+        criticalf("Could not read file at: %s", path);
+    }
+
+    return service;
 }
 
