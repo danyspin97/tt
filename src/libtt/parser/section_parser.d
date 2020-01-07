823879c727219e29e23d8d40adf4cd5a27ba6de8
Danilo Spinella <danyspin97@protonmail.com>
2020-01-07 22:49:11 +0100

    section_parser: Initial commit

---
+++ src/libtt/parser/section_parser.d
@@ -0,0 +1,95 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
+module libtt.parser.section_parser;
+
+import std.exception : enforce;
+import std.format : FormatException, formattedRead;
+import std.string : strip;
+
+class SectionParser
+{
+public:
+    @property string section() { return m_section; }
+    @property bool lineValid() { return m_valid; }
+
+    this(string line)
+    {
+        this.line = line;
+        parseLine();
+    }
+
+    unittest
+    {
+        auto line = "[foo]";
+        auto parser = new SectionParser(line);
+        assert(parser.lineValid);
+    }
+
+    unittest
+    {
+        auto line = "foo";
+        auto parser = new SectionParser(line);
+        assert(!parser.lineValid);
+    }
+
+    string getSectionOrDefault(string _default)
+    {
+        if (lineValid())
+        {
+            return section;
+        }
+
+        return _default;
+    }
+
+private:
+
+    // Only used for unit testing
+    this()
+    {
+
+    }
+
+    void parseLine()
+    {
+        try
+       {
+            tryParseLine();
+        }
+        catch (FormatException e)
+        {
+        }
+    }
+
+    void tryParseLine()
+    {
+        scope(success) m_valid = true;
+        scope(failure) m_valid = false;
+
+        line = strip(line);
+        line.formattedRead!"[%s]"(m_section);
+        enforce(m_section != "", "Section name cannot be empty.");
+    }
+
+    unittest
+    {
+        auto parser = new SectionParser();
+        parser.line = "[foo]";
+        import std.exception : assertNotThrown;
+        assertNotThrown!FormatException(parser.tryParseLine());
+        assert(parser.section == "foo");
+    }
+
+    unittest
+    {
+        auto parser = new SectionParser();
+        parser.line = "foo";
+        import std.exception : assertThrown;
+        assertThrown!FormatException(parser.tryParseLine());
+    }
+
+    string line;
+    string m_section;
+    bool m_valid;
+}
