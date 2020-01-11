bd0627cdb2e90053c183f98afebdc6a089c3e938
Danilo Spinella <danyspin97@protonmail.com>
2020-01-11 21:50:21 +0100

    service: Add a ServiceOptions attribute

--- src/libtt/services/service.d
+++ src/libtt/services/service.d
@@ -1,5 +1,7 @@
 module libtt.services.service;
 
+import libtt.services.service_options;
+
 abstract class Service
 {
 public:
@@ -7,17 +9,20 @@ public:
     @property string polishName() { return m_polishName; }
     @property string description() { return m_description; }
     @property string path() { return m_path; }
+    @property ServiceOptions options() { return m_options; }
 
     this (
         string name,
         string polishName,
         string description,
-        string path
+        string path,
+        ServiceOptions options
     ) {
         m_name = name;
         m_polishName = polishName;
         m_description = description;
         m_path = path;
+        m_options = options;
     }
 
     this (Service s)
@@ -33,4 +38,5 @@ private:
     string m_polishName;
     string m_description;
     string m_path;
+    ServiceOptions m_options;
 }
