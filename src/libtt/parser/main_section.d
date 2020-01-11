e2d7a77c4a159385b70d2810aeeb49341683fff9
Danilo Spinella <danyspin97@protonmail.com>
2020-01-11 21:39:20 +0100

    main_section: Initial commit

---
+++ src/libtt/parser/main_section.d
@@ -0,0 +1,14 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
+module libtt.parser.main_section;
+
+// Store Service attributes contained in the main section
+package struct MainSection
+{
+    string name;
+    string polishName;
+    string description;
+    string type;
+}
+
