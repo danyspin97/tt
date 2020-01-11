120ed0270b813c3a0546b8c0870daa73606415dc
Danilo Spinella <danyspin97@protonmail.com>
2020-01-11 21:46:12 +0100

    service_options: Initial commit

---
+++ src/libtt/services/service_options.d
@@ -0,0 +1,9 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
+module libtt.services.service_options;
+
+abstract class ServiceOptions
+{
+
+}
