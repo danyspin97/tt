7b455b7dfc90265d570bacea8fc2e55c0f8d5318
Danilo Spinella <danyspin97@protonmail.com>
2020-01-12 21:31:38 +0100

    oneshot,service: Add copyright notice

--- src/libtt/services/oneshot.d
+++ src/libtt/services/oneshot.d
@@ -1,3 +1,6 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
 module libtt.services.oneshot;
 
 import libtt.services.service : Service;
--- src/libtt/services/service.d
+++ src/libtt/services/service.d
@@ -1,3 +1,6 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
 module libtt.services.service;
 
 import libtt.services.service_options;
