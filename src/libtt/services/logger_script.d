7a6053f20469ac574043d4a6e910882365826092
Danilo Spinella <danyspin97@protonmail.com>
2020-01-12 21:30:05 +0100

    logger_script: Move class from Longrun

---
+++ src/libtt/services/logger_script.d
@@ -0,0 +1,40 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
+module libtt.services.logger_script;
+
+class LoggerScript : Script
+{
+public:
+    @property string destination() { return m_destination; }
+    @property uint maxsize() { return m_maxsize; }
+
+    this (
+        string execute,
+        string user,
+        string group,
+        Environment environment,
+        string destination,
+        uint maxsize,
+        Type type,
+        string shebang = ""
+    ) {
+        super(
+            getDefaultExecute(),
+            user,
+            group,
+            environment,
+            type,
+            shebang
+        );
+    }
+private:
+    string getDefaultExecute()
+    {
+        // TODO add default logger script
+        return "";
+    }
+
+    string m_destination;
+    uint m_maxsize;
+}
--- src/libtt/services/longrun.d
+++ src/libtt/services/longrun.d
@@ -1,46 +1,16 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
 module libtt.services.longrun;
 
 import libtt.services.service : Service;
 import libtt.services.script : Script;
 import libtt.services.environment : Environment;
+import libtt.services.logger_script : LoggerScript;
 
 class Longrun : Service
 {
 public:
-    class Logger : Script
-    {
-    public:
-        @property string destination() { return m_destination; }
-        @property uint maxsize() { return m_maxsize; }
-
-        this (
-            string execute,
-            string user,
-            string group,
-            Environment environment,
-            string destination,
-            uint maxsize,
-            Type type,
-            string shebang = ""
-        ) {
-            super(getDefaultExecute(),
-            user,
-            group,
-            environment,
-            type,
-            shebang);
-        }
-    private:
-        string getDefaultExecute()
-        {
-            // TODO add default logger script
-            return "";
-        }
-
-        string m_destination;
-        uint m_maxsize;
-    }
-
     @property ref const(Script) run () { return m_run; }
     @property ref const(Script) finish () { return m_finish; }
     @property ref const(Service[]) dependencies () { return m_depends; }
@@ -48,6 +18,7 @@ public:
 private:
     Script m_run;
     Script m_finish;
+    Logger m_logger;
 
     Service[] m_depends;
 }
