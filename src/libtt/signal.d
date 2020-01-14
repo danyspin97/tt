d9ff06df1b47dc088e70ad64983f607e95883856
Danilo Spinella <danyspin97@protonmail.com>
2020-01-14 16:39:52 +0100

    signal: Initial commit

---
+++ src/libtt/signal.d
@@ -0,0 +1,14 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
+module libtt.signal;
+
+enum Signal
+{
+    SIGHUP = 1,
+    SIGINT = 2,
+    SIGQUIT = 3,
+    SIGKILL = 9,
+    SIGTERM = 15
+}
+
