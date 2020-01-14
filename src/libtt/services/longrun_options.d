73b4046b7c2da3dc2052d97910a9b18835dde7bc
Danilo Spinella <danyspin97@protonmail.com>
2020-01-14 16:40:05 +0100

    longrun_options: Add options

--- src/libtt/services/longrun_options.d
+++ src/libtt/services/longrun_options.d
@@ -3,9 +3,48 @@
 
 module libtt.services.longrun_options;
 
+import std.exception : enforce;
+
 import libtt.services.service_options : ServiceOptions;
+import libtt.signal : Signal;
 
 class LongrunOptions : ServiceOptions
 {
+public:
+    @property uint notify() { return m_notify; }
+    @property void notify(uint notify) { m_notify = notify; }
 
+    @property uint timeoutFinish() { return m_timeoutFinish; }
+    @property void timeoutFinish(uint timeoutFinish)
+    {
+        m_timeoutFinish = timeoutFinish;
+    }
+
+    @property uint timeoutKill() { return m_timeoutKill; }
+    @property void timeoutKill(uint m_timeoutKill)
+    {
+        m_timeoutKill = m_timeoutKill;
+    }
+
+    @property Signal downSignal() { return m_downSignal; }
+    @property void downSignal(Signal downSignal)
+    {
+        m_downSignal = downSignal;
+    }
+
+    @property ushort maxDeath() { return m_maxDeath; }
+    @property void maxDeath(ushort maxDeath)
+    {
+        auto msg = "max_death property cannot be greater than 4096";
+        enforce(maxDeath < 4096, msg);
+        m_maxDeath = maxDeath;
+    }
+
+private:
+    uint m_notify;
+    uint m_timeoutFinish = 5000;
+    uint m_timeoutKill = 0;
+    Signal m_downSignal = Signal.SIGTERM;
+    ushort m_maxDeath = 3;
 }
+
