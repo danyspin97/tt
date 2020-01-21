5550f09b419b625808593db451856d8e45e82a17
Danilo Spinella <danyspin97@protonmail.com>
2020-01-22 00:06:11 +0100

    section_builder: Add interface SectionBuilder

---
+++ src/libtt/parser/section_builder.d
@@ -0,0 +1,9 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
+module libtt.parser.section_builder;
+
+interface SectionBuilder
+{
+    void parseLine(string line);
+}
