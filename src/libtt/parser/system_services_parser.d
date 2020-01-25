803c8a948b6ddd86a348b13ee855251c520a32b9
Danilo Spinella <danyspin97@protonmail.com>
2020-01-25 17:48:07 +0100

    system_services_parser: Add new class

---
+++ src/libtt/parser/system_services_parser.d
@@ -0,0 +1,17 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
+module libtt.parser.system_services_parser;
+
+import libtt.dirs : dirs;
+import libtt.parser.services_parser : ServicesParser;
+
+class SystemServicesParser : ServicesParser
+{
+public:
+    this()
+    {
+        paths = [ dirs.service, dirs.adminService ];
+        suffix = ".system";
+    }
+}
