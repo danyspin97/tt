133fb6de943f23957a4476c1f8045826c1a29ed0
Danilo Spinella <danyspin97@protonmail.com>
2020-01-25 15:12:16 +0100

    script_builder: Implement parsing

--- src/libtt/parser/script_builder.d
+++ src/libtt/parser/script_builder.d
@@ -3,7 +3,11 @@
 
 module libtt.parser.script_builder;
 
+import libtt.dirs : dirs;
+import libtt.parser.multiline_code_parser : MultilineCodeParser;
+import libtt.parser.key_value_parser : KeyValueParser;
 import libtt.parser.section_builder : SectionBuilder;
+import libtt.parser.utils : setFailsIfNotEmpty;
 import libtt.services.script : Script;
 import libtt.services.environment : Environment;
 
@@ -18,10 +22,117 @@ public:
 
     override void parseLine(string line)
     {
+        if (executeParser.isParsing())
+        {
+            // Continue parsing execute parameter
+            executeParser.parseLine(line);
+            return;
+        }
 
+        if (line == "")
+        {
+            return;
+        }
+
+        string key, value;
+        auto keyValueParser = new KeyValueParser(line);
+        if (keyValueParser.lineValid())
+        {
+            key = keyValueParser.key;
+            value = keyValueParser.value;
+            auto param = getParamByKey(key);
+            setFailsIfNotEmpty(param, value);
+            return;
+        }
+
+        if (executeParser.startParsing(line))
+        {
+            return;
+        }
+
+        throw new Exception(`Line "` ~ line ~ `" is not valid`);
     }
 
-private:
+    override void endParsing()
+    {
+        setShebangPerType(type);
+
+        script = new Script(
+            execute,
+            shebang,
+            environment
+        );
+
+        if (user != "")
+        {
+            script.user = user;
+        }
+
+        if (group != "")
+        {
+            script.group = group;
+        }
+    }
+
+protected:
+    // Only used for unittest
+    this() {}
+
+    string* getParamByKey(string key)
+    {
+        switch (key)
+        {
+            case "type":
+                return &type;
+            case "user":
+                return &user;
+            case "group":
+                return &group;
+            case "shebang":
+                return &shebang;
+            default:
+                auto errorMessage = `Camp named "` ~ key ~ `" is not allowed.`;
+                throw new Exception(errorMessage);
+        }
+    }
+
+    unittest
+    {
+        auto builder = new ScriptBuilder();
+        builder.type = "oneshot";
+        auto param = builder.getParamByKey("type");
+        assert(*param == "oneshot");
+        *param = "longrun";
+        assert(builder.type == "longrun");
+    }
+
+    void setShebangPerType(string type)
+    {
+        switch (type)
+        {
+            case "auto":
+                goto case;
+            case "execline":
+                auto newShebang = "#!" ~ dirs.execlinePrefix ~ "execlineb";
+                setFailsIfNotEmpty(&shebang, newShebang);
+                break;
+            case "bash":
+                auto newShebang = "#!" ~ dirs.bin ~ "bash";
+                setFailsIfNotEmpty(&shebang, newShebang);
+                break;
+            default:
+                auto errorMessage = `Type "` ~ type ~ `" is not allowed.`;
+                throw new Exception(errorMessage);
+        }
+    }
+
     Script script;
     Environment environment;
+    MultilineCodeParser executeParser = new MultilineCodeParser();
+
+    string type;
+    string shebang;
+    string user;
+    string group;
+    string execute;
 }
