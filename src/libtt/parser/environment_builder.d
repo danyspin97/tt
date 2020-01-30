9d496344c9d69867919a447eacc0c97d614b7c9e
Danilo Spinella <danyspin97@protonmail.com>
2020-01-30 18:07:45 +0100

    environment_builder: Implement parsing

--- src/libtt/parser/environment_builder.d
+++ src/libtt/parser/environment_builder.d
@@ -3,6 +3,8 @@
 
 module libtt.parser.environment_builder;
 
+import std.regex : ctRegex, matchAll;
+
 import libtt.parser.section_builder : SectionBuilder;
 import libtt.services.environment : Environment;
 
@@ -16,7 +18,14 @@ public:
 
     override void parseLine(string line)
     {
+        auto regex = ctRegex!r" *(\w+) *= *(.*) *";
+        auto match = matchAll(line, regex);
+        if (!match)
+        {
+            throw new Exception(`Line "` ~ line ~ `" is not valid`);
+        }
 
+        environment.set(match.front[1], match.front[2]);
     }
 
     override void endParsing()
