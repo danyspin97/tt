62337e42f6b0b6c5d1f41179abf3274611ee6a1f
Danilo Spinella <danyspin97@protonmail.com>
2020-01-30 18:05:55 +0100

    service_parser: Use class InstanceServiceParser
    Check if services are instanced services and dispatch them to
    InstanceServiceParser if required.

--- src/libtt/parser/services_parser.d
+++ src/libtt/parser/services_parser.d
@@ -3,9 +3,12 @@
 
 module libtt.parser.services_parser;
 
+import std.array : split;
 import std.file : dirEntries, exists, isFile, SpanMode;
+import std.regex : ctRegex, matchFirst;
 
 import libtt.services.service : Service;
+import libtt.parser.instance_service_parser : InstanceServiceParser;
 import libtt.parser.service_parser : ServiceParser;
 
 abstract class ServicesParser
@@ -40,8 +43,35 @@ private:
             return;
         }
 
+        if (isInstancedService(serviceName))
+        {
+            string instanceName;
+            splitServiceNameFromInstance(serviceName, instanceName);
+            auto path = getPathForServiceName(serviceName);
+            serviceSet[serviceName ~ instanceName] =
+                    new InstanceServiceParser(path, instanceName).service;
+            return;
+        }
+
         auto path = getPathForServiceName(serviceName);
         serviceSet[serviceName] = new ServiceParser(path).service;
+    }
+
+    bool isInstancedService(string serviceName)
+    {
+        auto regex = ctRegex!r"[\w-]+@[\w-]+";
+        if (matchFirst(serviceName, regex))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void splitServiceNameFromInstance(ref string service, ref string instance)
+    {
+        auto splitted = service.split("@");
+        service = splitted[0];
+        instance = splitted[1];
     }
 
     string getPathForServiceName(string name)
