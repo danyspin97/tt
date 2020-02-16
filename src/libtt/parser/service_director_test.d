e838be178d738d47bcf8ae7e81cb0cd817843351
Danilo Spinella <danyspin97@protonmail.com>
2020-02-16 12:27:08 +0100

    service_director_test: Add new class for tests
    Add new class to test parseSections() method of
    ServiceDirector, which is an abstract class.

---
+++ src/libtt/parser/service_director_test.d
@@ -0,0 +1,67 @@
+// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
+// Distributed under the terms of the GNU General Public License v2
+
+module libtt.parser.service_director_test;
+
+import std.container : DList;
+
+import libtt.parser.section_builder : SectionBuilder;
+import libtt.parser.service_director : ServiceDirector;
+import libtt.services.service : Service;
+
+class ServiceDirectorTest : ServiceDirector
+{
+    class BuilderTest : SectionBuilder
+    {
+        void parseLine(string line)
+        {
+            timesParsed++;
+        }
+
+        void endParsing()
+        {
+            parsingEnded = true;
+        }
+
+        uint timesParsed;
+        bool parsingEnded = false;
+    }
+
+protected:
+    override Service instanceService(string path)
+    {
+        return null;
+    }
+
+    override SectionBuilder getBuilderForSection(string section)
+    {
+        builder = new BuilderTest();
+        return builder;
+    }
+
+    unittest
+    {
+        auto director = new ServiceDirectorTest();
+        auto s = DList!string("", "[foo]");
+        auto linesNumber = 10;
+        foreach (i; 0 .. linesNumber)
+        {
+            s.insertBack("");
+        }
+
+        director.parseAndGetService(s, "");
+        assert(director.builder.timesParsed == linesNumber);
+        assert(director.builder.parsingEnded);
+    }
+
+    unittest
+    {
+        auto director = new ServiceDirectorTest();
+        auto s = DList!string("foo", "");
+        import std.exception : assertThrown;
+        assertThrown!Exception(director.parseAndGetService(s, ""));
+    }
+
+    BuilderTest builder;
+}
+
