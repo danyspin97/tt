43f63efe86949d6ac1b858a60d97ed1b40a2fe35
Danilo Spinella <danyspin97@protonmail.com>
2020-02-16 20:15:11 +0100

    main_section: Fix module definition and visibility

--- src/libtt/parser/section/main_section.d
+++ src/libtt/parser/section/main_section.d
@@ -1,10 +1,10 @@
 // Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
 // Distributed under the terms of the GNU General Public License v2
 
-module libtt.parser.main_section;
+module libtt.parser.section.main_section;
 
 // Store Service attributes contained in the main section
-package struct MainSection
+struct MainSection
 {
     string name;
     string polishName;
