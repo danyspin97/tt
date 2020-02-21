478705832507ff7c3c62747c1abbded1abd616d2
Danilo Spinella <danyspin97@protonmail.com>
2020-02-21 17:56:29 +0100

    multiline_value_parser: Rework class

--- src/libtt/parser/line/multiline_value_parser.d
+++ src/libtt/parser/line/multiline_value_parser.d
@@ -4,18 +4,20 @@
 module libtt.parser.line.multiline_value_parser;
 
 import std.algorithm : each, endsWith, filter, startsWith;
-import std.array : array;
+import std.array : split;
 import std.exception : assertNotThrown, assertThrown;
 import std.range : popFrontN, popBackN;
-import std.regex : ctRegex, matchAll, splitter;
 import std.string: strip;
 import std.uni : isWhite;
 
+import libtt.parser.word : KeyParser, TokenParser, UntilTokenParser,
+                           WhitespaceParser;
+
 import libtt.exception : EmptyValueFoundWhileParsingException,
+                         WordNotValidException,
                          LineNotValidWhileParsingException,
                          ParserHasNotBeenInitialized,
-                         ParserIsStillParsingException,
-                         ValueNotValidWhileParsing;
+                         ParserIsStillParsingException;
 
 class MultilineValueParser
 {
@@ -36,26 +38,14 @@ public:
             throw new ParserIsStillParsingException("");
         }
 
-        auto regex = ctRegex!r" *(\w+) *= *(\( *)(\w+(:?-\w+)? +)*( *\))? *";
-        auto match = matchAll(line, regex);
-        if (!match)
+        try
+        {
+            tryStartParse(line);
+        }
+        catch (WordNotValidException e)
         {
             return false;
         }
-
-        auto sub = match.front;
-        // The first value (sub[0]) is the entire line
-        m_key = sub[1];
-        auto openingParenthesis = sub[2];
-        sub.popFrontN(3);
-        setValues(sub);
-        m_isParsing = true;
-        if (m_values.length != 0)
-        {
-            checkEndingParenthesis();
-        }
-        checkOpeningParenthesis(openingParenthesis);
-
         return true;
     }
 
@@ -83,20 +73,18 @@ public:
         );
     }
 
-    // unittest
-    // {
-    //     auto parser = new MultilineValueParser();
-    //     assertThrown!LineNotValidWhileParsingException(
-    //         parser.startParsing(`foo = ( foo)`)
-    //     );
-    // }
+    unittest
+    {
+        auto parser = new MultilineValueParser();
+        assertThrown!LineNotValidWhileParsingException(
+            parser.startParsing(`foo = ( foo)`)
+        );
+    }
 
     unittest
     {
         auto linesWithEmptyValues =
         [
-            "foo = ()",
-            "foo = ( )",
             "foo = (  )"
         ];
         foreach (line ; linesWithEmptyValues)
@@ -114,21 +102,7 @@ public:
             throw new ParserHasNotBeenInitialized("");
         }
 
-        auto regex = ctRegex!r"^ *(\w+(:?-\w+)? +)*( *\))? *$";
-        auto match = matchAll(line, regex);
-        if (!match)
-        {
-            throw new LineNotValidWhileParsingException("");
-        }
-
-        auto sub = match.front;
-        sub.popFrontN(1);
-        auto previousNumOfValues = m_values.length;
-        setValues(sub);
-        if (previousNumOfValues != m_values.length)
-        {
-            checkEndingParenthesis();
-        }
+        tryParseLine(line);
     }
 
     unittest
@@ -152,14 +126,6 @@ public:
         assertThrown!ParserHasNotBeenInitialized(parser.parseLine(line));
     }
 
-    unittest
-    {
-        auto parser = new MultilineValueParser();
-        auto line = "foo = bar";
-        parser.m_isParsing = true;
-        assertThrown!LineNotValidWhileParsingException(parser.parseLine(line));
-    }
-
     void reset()
     {
         m_values = [];
@@ -180,136 +146,123 @@ public:
     }
 
 private:
-    static void checkOpeningParenthesis(string openingParenthesis)
+    bool tryStartParse(string line)
     {
-        if (openingParenthesis.startsWith("(") && openingParenthesis == "(")
+        auto keyParser = new KeyParser();
+        auto assignmentParser = new TokenParser('=');
+        auto openParenthesisParser = new TokenParser('(');
+        line = keyParser.parse(line);
+        line = assignmentParser.parse(line);
+        line = openParenthesisParser.parse(line);
+        checkLineStartsWithWhitespace(line);
+
+        auto res = !tryParseLine(line);
+        checkParsedValuesNotEmpty();
+        return res;
+    }
+
+    bool tryParseLine(string line)
+    {
+        try
         {
-            auto msg = "A space is needed after the parenthesis";
-            throw new LineNotValidWhileParsingException(msg);
+            tryParseValuesInLine(line);
+        }
+        catch (WordNotValidException e)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    void tryParseValuesInLine(string line)
+    {
+        scope(failure) addValuesFromLine(line);
+        scope(failure) m_isParsing = true;
+        scope(success) m_isParsing = false;
+
+        auto endingParenthesisParser = new UntilTokenParser(')');
+        line = endingParenthesisParser.parseUntilToken(line);
+        auto parsedLine = endingParenthesisParser.parsedLine;
+        checkLineEndsWithWhitespace(parsedLine);
+        addValuesFromLine(parsedLine);
+    }
+
+    static void checkLineStartsWithWhitespace(string line)
+    {
+        // A space is needed before the parenthesis
+        if (line.length != 0 && !line[0].isWhite())
+        {
+            throw new LineNotValidWhileParsingException("");
         }
     }
 
     unittest
     {
         assertNotThrown!LineNotValidWhileParsingException(
-            checkOpeningParenthesis("( ")
+            checkLineStartsWithWhitespace(" foo")
         );
     }
 
-    void setValues(R)(R matches)
-    {
-        matches.filter!(s => s != "").
-                each!(s => m_values ~= s.strip());
-    }
-
     unittest
     {
-        auto parser = new MultilineValueParser();
-        auto line = ["foo", "bar"];
-        parser.setValues(line[]);
-        assert(parser.values == ["foo", "bar"]);
-    }
-
-    unittest
-    {
-        auto parser = new MultilineValueParser();
-        auto line = ["foo ", "", "bar"];
-        parser.setValues(line[]);
-        assert(parser.values == ["foo", "bar"]);
-    }
-
-    void checkEndingParenthesis()
-    {
-        if (m_values.length == 0)
-        {
-            return;
-        }
-
-        string last = m_values[$-1];
-        if (last.endsWith(")"))
-        {
-            if (m_values.length == 1)
-            {
-                throw new EmptyValueFoundWhileParsingException("");
-            }
-            if (last == ")")
-            {
-                auto msg = "A space is needed before the parenthesis";
-                throw new LineNotValidWhileParsingException(msg);
-            }
-            m_values = m_values[0 .. $-1];
-            m_isParsing = false;
-        }
-    }
-
-    unittest
-    {
-        auto parser = new MultilineValueParser();
-        auto testValues = [
-            [ "foo", " )" ],
-            [ "foo" ]
-        ];
-        foreach (values ; testValues)
-        {
-            parser.m_values = values;
-            assertNotThrown!EmptyValueFoundWhileParsingException(
-                parser.checkEndingParenthesis()
-            );
-            parser.m_values = values;
-            assertNotThrown!LineNotValidWhileParsingException(
-                parser.checkEndingParenthesis()
-            );
-            assert(parser.values == ["foo"]);
-        }
-    }
-
-    unittest
-    {
-        auto parser = new MultilineValueParser();
-        parser.m_isParsing = true;
-        auto testValues = [
-            [ "foo" ],
-            []
-        ];
-        foreach (values ; testValues)
-        {
-            parser.m_values = values;
-            parser.checkEndingParenthesis();
-            assert(parser.isParsing());
-        }
-    }
-
-    unittest
-    {
-        auto parser = new MultilineValueParser();
-        parser.m_values = ["foo", ")"];
-        parser.checkEndingParenthesis();
-        assert(!parser.isParsing());
-    }
-
-    unittest
-    {
-        auto testValues = [
-            [ ")" ],
-            [ " )" ],
-        ];
-        foreach (values ; testValues)
-        {
-            auto parser = new MultilineValueParser();
-            parser.m_values = values;
-            assertThrown!EmptyValueFoundWhileParsingException(
-                parser.checkEndingParenthesis()
-            );
-        }
-    }
-
-    unittest
-    {
-        auto parser = new MultilineValueParser();
-        parser.m_values = ["foo", ")"];
         assertThrown!LineNotValidWhileParsingException(
-            parser.checkEndingParenthesis()
+            checkLineStartsWithWhitespace("foo")
         );
+    }
+
+    static void checkLineEndsWithWhitespace(string line)
+    {
+        // A space is needed after the parenthesis
+        if (line.length == 0 || !line[$-1].isWhite())
+        {
+            throw new LineNotValidWhileParsingException("");
+        }
+    }
+
+    unittest
+    {
+        assertThrown!LineNotValidWhileParsingException(
+            checkLineEndsWithWhitespace("")
+        );
+    }
+
+    unittest
+    {
+        assertNotThrown!LineNotValidWhileParsingException(
+            checkLineStartsWithWhitespace("        ")
+        );
+    }
+
+    void addValuesFromLine(string line)
+    {
+        line.split!isWhite
+            .filter!(s => s != "")
+            .each!(s => m_values ~= s.strip());
+    }
+
+    unittest
+    {
+        auto parser = new MultilineValueParser();
+        auto line = "foo bar";
+        parser.addValuesFromLine(line);
+        assert(parser.values == ["foo", "bar"]);
+    }
+
+    unittest
+    {
+        auto parser = new MultilineValueParser();
+        auto line = "foo    bar ";
+        parser.addValuesFromLine(line);
+        assert(parser.values == ["foo", "bar"]);
+    }
+
+    void checkParsedValuesNotEmpty()
+    {
+        if (!isParsing() && m_values.length == 0)
+        {
+            throw new EmptyValueFoundWhileParsingException("");
+        }
     }
 
     string m_key;
