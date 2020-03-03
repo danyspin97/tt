b05da84f44c01a408ffbb02630724847c7d4b06d
Danilo Spinella <danyspin97@protonmail.com>
2020-03-03 18:38:54 +0100

    oneshot_options_builder: Update interface

--- src/libtt/parser/section/oneshot_options_builder.d
+++ src/libtt/parser/section/oneshot_options_builder.d
@@ -6,7 +6,7 @@ module libtt.parser.section.oneshot_options_builder;
 @safe:
 nothrow:
 
-import libtt.exception : BooleanParseException, BuilderException;
+import libtt.parser.section.utils : attributeNotFound;
 import libtt.parser.section.options_builder : OptionsBuilder;
 import libtt.parser.line : MultilineValueParser;
 import libtt.parser.utils : parseBoolean;
@@ -21,16 +21,17 @@ public:
     }
 
 protected:
-    override void setParamByKey(in string key, in string value)
+    override void trySetAttributeForKey(in string key, in string value)
     {
-        try
+        switch (key)
         {
-            trySetParamByKey(key, value);
-        }
-        catch (BooleanParseException e)
-        {
-            const auto msg = e.msg ~ ` while parsing key "` ~ key ~ `"`;
-            throw new BuilderException(msg);
+        case "optional":
+            oneshotOptions.optional = parseBoolean(value);
+            break;
+        case "write_message":
+            oneshotOptions.writeMessage = parseBoolean(value);
+            break;
+        default:
         }
     }
 
@@ -42,28 +43,10 @@ protected:
             oneshotOptions.dependencies = parser.values;
             break;
         default:
-            const auto errorMessage = `Camp named "` ~ parser.key
-                ~ `" is not allowed in section [options]`;
-            throw new Exception(errorMessage);
+            attributeNotFound(parser.key, "options");
         }
     }
 
 private:
-    void trySetParamByKey(in string key, in string value) @safe
-    {
-        switch (key)
-        {
-        case "optional":
-            oneshotOptions.optional = parseBoolean(value);
-            break;
-        case "write_message":
-            oneshotOptions.writeMessage = parseBoolean(value);
-            break;
-        default:
-            auto errorMessage = `Camp named "` ~ key ~ `" is not allowed` ~ " in section [options]";
-            throw new Exception(errorMessage);
-        }
-    }
-
     OneshotOptions oneshotOptions;
 }
