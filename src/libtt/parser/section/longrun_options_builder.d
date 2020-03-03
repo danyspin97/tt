bf1f8a33c782f7126c79a938667d7b93832627e2
Danilo Spinella <danyspin97@protonmail.com>
2020-03-03 18:39:37 +0100

    longrun_options_builder: Implement logic

--- src/libtt/parser/section/longrun_options_builder.d
+++ src/libtt/parser/section/longrun_options_builder.d
@@ -6,27 +6,83 @@ module libtt.parser.section.longrun_options_builder;
 @safe:
 nothrow:
 
-import libtt.parser.section.section_builder : SectionBuilder;
-import libtt.data : LongrunOptions;
+import std.conv : to;
 
-class LongrunOptionsBuilder : SectionBuilder
+import libtt.data : LongrunOptions;
+import libtt.exception : BuilderException;
+import libtt.parser.line : MultilineValueParser;
+import libtt.parser.section.options_builder : OptionsBuilder;
+import libtt.signal : Signal, parseSignalFromString;
+import libtt.parser.utils : parseBoolean;
+import libtt.parser.section.utils : attributeNotFound, testBuilderWithFile;
+
+class LongrunOptionsBuilder : OptionsBuilder
 {
 public:
-    this(in LongrunOptions longrunOptions)
+    this(LongrunOptions longrunOptions)
     {
         this.longrunOptions = longrunOptions;
     }
 
-    override void parseLine(string line)
+    @system unittest
     {
-
+        LongrunOptions o = new LongrunOptions();
+        auto builder = new LongrunOptionsBuilder(o);
+        builder.testBuilderWithFile("src/libtt/test/longrun_options_section");
     }
 
-    override void endParsing()
+    @system unittest
     {
+        LongrunOptions o = new LongrunOptions();
+        auto builder = new LongrunOptionsBuilder(o);
+        import std.exception : assertThrown;
 
+        assertThrown!BuilderException(
+                builder.testBuilderWithFile("src/libtt/test/invalid_int_value"));
+    }
+
+    override void saveValuesOfParser(ref MultilineValueParser parser)
+    {
+        switch (parser.key)
+        {
+        case "depends":
+            longrunOptions.dependencies = parser.values;
+            break;
+        default:
+            attributeNotFound(parser.key, "options");
+        }
+    }
+
+    override void trySetAttributeForKey(in string key, in string value)
+    {
+        switch (key)
+        {
+        case "down_signal":
+            longrunOptions.downSignal = parseSignalFromString(value);
+            break;
+        case "maxdeath":
+            longrunOptions.maxDeath = to!ushort(value);
+            break;
+        case "notify":
+            longrunOptions.notify = to!uint(value);
+            break;
+        case "optional":
+            longrunOptions.optional = parseBoolean(value);
+            break;
+        case "timeout_finish":
+            longrunOptions.timeoutFinish = to!uint(value);
+            break;
+        case "timeout_kill":
+            longrunOptions.timeoutKill = to!uint(value);
+            break;
+        case "write_message":
+            longrunOptions.writeMessage = parseBoolean(value);
+            break;
+        default:
+            attributeNotFound(key, "options");
+        }
     }
 
 private:
-    const LongrunOptions longrunOptions;
+    LongrunOptions longrunOptions;
 }
