107827dc5bae252b05810618bb85ef15ffad2568
Danilo Spinella <danyspin97@protonmail.com>
2020-03-05 17:25:44 +0100

    bundle_options_builder: Implement Builder

--- src/libtt/parser/section/bundle_options_builder.d
+++ src/libtt/parser/section/bundle_options_builder.d
@@ -6,27 +6,62 @@ module libtt.parser.section.bundle_options_builder;
 @safe:
 nothrow:
 
-import libtt.parser.section.section_builder : SectionBuilder;
 import libtt.data : BundleOptions;
+import libtt.exception : BuilderException;
+import libtt.parser.line : MultilineValueParser;
+import libtt.parser.section.options_builder : OptionsBuilder;
+import libtt.parser.section.utils : attributeNotFound, isEmptyLine, testBuilderWithFile;
 
-class BundleOptionsBuilder : SectionBuilder
+class BundleOptionsBuilder : OptionsBuilder
 {
 public:
-    this(BundleOptions bundleOptions)
+    this(ref BundleOptions options)
     {
-        this.bundleOptions = bundleOptions;
+        this.options = options;
+    }
+
+    @system unittest
+    {
+        auto o = new BundleOptions;
+        auto builder = new BundleOptionsBuilder(o);
+        builder.testBuilderWithFile("src/libtt/test/bundle_options_section");
+
+        assert(o.contents == ["foo", "bar"]);
     }
 
     override void parseLine(string line)
     {
+        if (isEmptyLine(line))
+        {
+            return;
+        }
 
+        if (parseMultilineValue(line))
+        {
+            return;
+        }
+
+        throw new BuilderException("Line " ~ line ~ " is not valid in section [options]");
     }
 
-    override void endParsing()
+protected:
+    override void saveValuesOfParser(ref MultilineValueParser parser)
     {
+        switch (parser.key)
+        {
+        case "contents":
+            options.contents = parser.values;
+            break;
+        default:
+            attributeNotFound(parser.key, "options");
+        }
+    }
 
+    override void trySetAttributeForKey(string key, string value)
+    {
+        throw new BuilderException("No key is allowed");
     }
 
 private:
-    BundleOptions bundleOptions;
+    BundleOptions options;
 }
