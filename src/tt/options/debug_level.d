d0f984d57670fab46b3d1e1ee042d37a5d84b97f
Danilo Spinella <danyspin97@protonmail.com>
2020-03-07 17:55:44 +0100

    debug_level: Add module declaration

--- src/tt/options/debug_level.d
+++ src/tt/options/debug_level.d
@@ -1,6 +1,8 @@
 // Copyright 2020 Rasmus Thomsen <oss@cogitri.dev>
 // Distributed under the terms of the GNU General Public License v2
 
+module tt.options.debug_level;
+
 /// The debug level to run the `tt` binary in
 enum DebugLevel
 {
