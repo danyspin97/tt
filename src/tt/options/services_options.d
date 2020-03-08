6d779ce14072dd4eebf2a38dd34f116e101b4cea
Danilo Spinella <danyspin97@protonmail.com>
2020-03-08 12:29:16 +0100

    services_options: Add new module

---
+++ src/tt/options/services_options.d
@@ -0,0 +1,31 @@
+// Copyright 2020 Rasmus Thomsen <oss@cogitri.dev>
+// Distributed under the terms of the GNU General Public License v2
+
+module tt.options.services_options;
+
+import tt.options.options : Options;
+import tt.options.common_options : CommonOptions;
+
+@safe:
+nothrow:
+
+abstract class ServicesOptions : Options
+{
+public:
+    @property string[] services() { return m_services; }
+
+    this(in CommonOptions commonOptions, string[] args)
+    {
+        super(commonOptions, args);
+    }
+
+protected:
+    void parseServices()
+    {
+        m_services = args[2 .. $];
+    }
+
+private:
+    string[] m_services;
+}
+
