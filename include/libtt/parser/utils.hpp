64b65201329ba1f8e734e5b144c6ab411939a2b6
Danilo Spinella <danyspin97@protonmail.com>
2020-03-19 23:14:54 +0100

    parser/utils.hpp: Apply style fixes

--- include/libtt/parser/utils.hpp
+++ include/libtt/parser/utils.hpp
@@ -7,27 +7,26 @@
 // Thanks StackOverflow!
 // https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
 
-#include <algorithm> 
+#include <algorithm>
 #include <cctype>
 #include <locale>
 
 // trim from start (in place)
 static inline void ltrim(std::string &s) {
-s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
-return !std::isspace(ch);
-}));
-    }
-    // trim from end (in place)
-     static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
-    return !std::isspace(ch);
-    }).base(), s.end());
-    }
-        // trim from both ends (in place)
-    static inline void trim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+                                    [](int ch) { return !std::isspace(ch); }));
+}
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+                         [](int ch) { return !std::isspace(ch); })
+                .base(),
+            s.end());
+}
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
     ltrim(s);
     rtrim(s);
-    }
+}
 
 #endif // LIBTT_PARSER_UTILS_HPP_
-
