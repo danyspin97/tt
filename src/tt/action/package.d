34f6d9e4754c957a3e61116e11d3aa0c6b765c76
Danilo Spinella <danyspin97@protonmail.com>
2020-03-25 11:50:40 +0100

    Remove package.d modules and dub files

--- src/tt/action/package.d
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 Danilo Spinella <danyspin97@protonmail.com>
-// Distributed under the terms of the GNU General Public License v2
-
-module tt.action;
-
-public import tt.action.action;
-public import tt.action.action_factory;
-public import tt.action.parse_action;
