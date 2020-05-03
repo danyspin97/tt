cf706f5206f28c21627580f16877bc7a711a10f2
Danilo Spinella <danyspin97@protonmail.com>
2020-05-03 19:24:07 +0200

    Convert and rename Action to Command

---
+++ include/tt/cli/command/command.hpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <danyspin97@protonmail.com>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include "tt/cli/global_options.hpp"
+
+namespace args {
+class Subparser;
+}
+
+namespace tt {
+
+namespace cli {
+
+class Command {
+public:
+    Command(args::Subparser &parser,
+            std::shared_ptr<GlobalOptions> common_options);
+    virtual ~Command() = default;
+
+    int InitAndExecute();
+
+protected:
+    virtual int Execute() = 0;
+
+protected:
+    args::Subparser &parser_;
+    std::shared_ptr<GlobalOptions> common_options_;
+};
+
+} // namespace cli
+} // namespace tt
---
+++ src/cli/command/command.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <danyspin97@protonmail.com>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tt/cli/command/command.hpp"
+
+#include <memory>
+
+#include "args.hxx"
+
+using std::shared_ptr;
+
+using tt::cli::Command;
+using tt::cli::GlobalOptions;
+
+Command::Command(args::Subparser &parser,
+                 shared_ptr<GlobalOptions> common_options)
+    : parser_(parser), common_options_(common_options) {}
+
+int Command::InitAndExecute() {
+    parser_.Parse();
+    return Execute();
+}
