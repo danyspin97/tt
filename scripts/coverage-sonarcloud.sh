8fce11e1f58b09f78d14533e9a189fe192948ac4
Danilo Spinella <danyspin97@protonmail.com>
2020-06-18 13:41:31 +0200

    Add SonarCloud analysis in Travis CI

---
+++ scripts/coverage-sonarcloud.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+GCOV_DIR="${MESON_SOURCE_ROOT}/gcov"
+mkdir "${GCOV_DIR}"
+
+cd "${MESON_BUILD_ROOT}"
+gcov-9 -p src/*/*.gcno > /dev/null
+gcov-9 -p test/*/*.gcno > /dev/null
+mv *.gcov "${GCOV_DIR}"
