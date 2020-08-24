f72b8727d29d82cf249bf1e955050c5adfceb019
Danilo Spinella <oss@danyspin97.org>
2020-08-24 15:19:35 +0200

    SpawnScript: Implement spawning using pstream.h
    Include svc/spawn_script.cpp source in "tt" and test executable.
    By doing so, it is possible to implement signal handling and logger
    declaration in these sources.
    
    https://stackoverflow.com/questions/30400286/logging-from-library-with-spdlog

--- test/svc/spawn_script.cpp
+++ test/svc/spawn_script.cpp
@@ -1,16 +1,41 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <danyspin97@protonmail.com>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include "tt/supervisor/spawn_script.hpp"
-#include "tt/supervisor/types.hpp"
+#include "tt/svc/spawn_script.hpp"
 
 #include "catch2/catch.hpp"
+
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
 
 #include <unistd.h>
 
 TEST_CASE("SpawnScript") {
+    spdlog::set_level(spdlog::level::debug);
+    spdlog::stdout_color_mt("oneshot");
+
     SECTION("Spawn sucessfull script") {
         tt::Environment env;
-        tt::Script script{tt::Script::Type::Bash, "echo tt"};
-        tt::SpawnScript spawn_script(script, env, [](std::string) {});
+        tt::Script script{tt::Script::Type::Bash, "exit 0"};
+        tt::SpawnScript spawn_script("test-script", script, env,
+                                     [](std::string) {});
         tt::ScriptStatus status = spawn_script.Spawn();
         CHECK(status == tt::ScriptStatus::Success);
     }
