6129bed6572302fa4409619509f393ced9c17325
Danilo Spinella <oss@danyspin97.org>
2020-09-01 19:20:10 +0200

    SpawnSupervise: Add license header

--- test/svc/spawn_supervise.cpp
+++ test/svc/spawn_supervise.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <danyspin97@protonmail.com>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "tt/svc/spawn_supervise.hpp"
 #include "tt/svc/types.hpp"
