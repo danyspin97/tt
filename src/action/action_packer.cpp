3091ad60db59dc28b3a1f76e64147bdec9cb0758
Danilo Spinella <oss@danyspin97.org>
2020-09-27 23:25:53 +0200

    ActionPacker: Add initial implementation

---
+++ src/action/action_packer.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <danyspin97@protonmail.com>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tt/action/action_packer.hpp"
+
+#include "msgpack.hpp"
+
+#include "nngpp/buffer.h"
+
+#include "tt/action/adapter/notify_up_action_adapter.hpp"
+#include "tt/action/notify_up_action.hpp"
+
+auto tt::ActionPacker::PackCommand(const char *command) -> std::stringstream {
+    std::stringstream stream;
+    msgpack::pack(stream, command);
+    return stream;
+}
+
+auto tt::ActionPacker::Pack(tt::NotifyUpAction &&action) -> nng::buffer {
+    auto stream = PackCommand("notify_up");
+
+    msgpack::pack(stream, action);
+    nng::buffer buffer{stream.str().c_str(), stream.str().size()};
+    return buffer;
+}
