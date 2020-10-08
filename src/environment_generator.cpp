237a71dc5ca1b964515ca246af971489e05b798d
Danilo Spinella <oss@danyspin97.org>
2020-10-08 15:18:03 +0200

    EnvironmentGenerator: Add initial implementation

---
+++ src/environment_generator.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <oss@danyspin97.org>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tt/environment_generator.hpp"
+
+auto tt::EnvironmentGenerator::GetEnvironment() -> std::vector<const char *> {
+    // TODO: Read from /etc/tt/tt.env
+    std::vector<const char *> env;
+    env.push_back("PATH=/usr/bin/");
+    env.push_back(0);
+    return env;
+}
