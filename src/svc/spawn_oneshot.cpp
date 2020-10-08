7c19356cc4b6334a39297e39a7dbce2988b3e6b5
Danilo Spinella <oss@danyspin97.org>
2020-10-08 15:26:28 +0200

    SpawnOneshot: Add initial implementation

---
+++ src/svc/spawn_oneshot.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <oss@danyspin97.org>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tt/svc/spawn_oneshot.hpp"
+
+#include "tt/svc/service_status_manager.hpp"
+#include "tt/svc/spawn_script.hpp"
+
+tt::SpawnOneshot::SpawnOneshot(const Oneshot &oneshot) {
+    const auto &name = oneshot.name();
+    SpawnScript spawn_start{name, oneshot.start(), oneshot.environment()};
+    if (spawn_start.Spawn() == ScriptStatus::Success) {
+        // Notify the service started up successfully
+        auto manager = ServiceStatusManager::GetInstance();
+        manager.ServiceHasStarted(name);
+        return;
+    }
+    if (oneshot.stop()) {
+        SpawnScript spawn_stop{name, oneshot.stop().value(),
+                               oneshot.environment()};
+        // We don't care if the stop script failed
+        spawn_stop.Spawn();
+    }
+}
