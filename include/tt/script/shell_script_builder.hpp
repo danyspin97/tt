4f597d8c07d63b7e17d1cf2e97b1fd1f0e4474a7
Danilo Spinella <oss@danyspin97.org>
2020-10-12 22:49:30 +0200

    ShellScriptBuilder: Add initial implementation

---
+++ include/tt/script/shell_script_builder.hpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <oss@danyspin97.org>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <array>
+#include <filesystem>
+#include <string>
+
+#include "tt/data/environment.hpp"
+
+namespace tt {
+
+class ShellScripterBuilder {
+public:
+    ShellScripterBuilder(const std::string &execute,
+                         const Environment &environment);
+    virtual ~ShellScripterBuilder();
+
+    [[nodiscard]] auto environment() const -> const Environment &;
+
+    // Returns the file to run (i.e. /bin/sh) and its arguments
+    // (i.e. "sh", "-c", "execute...")
+    [[nodiscard]] auto script()
+        -> std::pair<const char *, std::array<const char *, 3>>;
+
+protected:
+    // No newline are applied
+    void AppendCode(const std::string &code);
+    void PrependCode(const std::string &code);
+
+    // Use AppendCode() and PrependCode() to add code to the execute script
+    virtual void ApplyModifiers() = 0;
+    // Return the shell to execute, like /bin/bash
+    // Must be absolute
+    [[nodiscard]] virtual std::filesystem::path GetFileToExecute() = 0;
+
+private:
+    std::string script_;
+    Environment env_;
+
+    bool builded_ = false;
+};
+
+} // namespace tt
---
+++ src/script/shell_script_builder.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <oss@danyspin97.org>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tt/script/shell_script_builder.hpp"
+
+#include "fmt/format.h"
+
+tt::ShellScripterBuilder::ShellScripterBuilder(const std::string &execute,
+                                               const Environment &environment)
+    : script_(execute), env_(environment) {}
+
+auto tt::ShellScripterBuilder::environment() const -> const Environment & {
+    return env_;
+}
+
+auto tt::ShellScripterBuilder::script()
+    -> std::pair<const char *, std::array<const char *, 3>> {
+    if (!builded_) {
+        ApplyModifiers();
+        builded_ = true;
+    }
+
+    // This leaks, but it will be passed to execvp
+    auto *bin = new std::filesystem::path{GetFileToExecute()};
+    assert(bin->is_absolute());
+
+    auto shell = new std::filesystem::path{bin->filename()};
+    return std::make_pair(
+        bin->c_str(),
+        std::array<const char *, 3>{shell->c_str(), "-c",
+                                    (new std::string{script_})->c_str()});
+}
+
+void tt::ShellScripterBuilder::AppendCode(const std::string &code) {
+    fmt::format_to(std::back_inserter(script_), "{}", code);
+}
+void tt::ShellScripterBuilder::PrependCode(const std::string &code) {
+    std::string new_script;
+    fmt::format_to(std::back_inserter(new_script), "{}{}", code, script_);
+    script_ = std::move(new_script);
+}
