9247287f0ec4dcc20959f6a3002fc88ffd4ce291
Danilo Spinella <oss@danyspin97.org>
2020-10-14 16:59:05 +0200

    FakeTimeout: Add initial implementation

---
+++ include/tt/svc/fake_timeout.hpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <oss@danyspin97.org>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "tt/svc/timeout.hpp"
+
+namespace tt {
+
+class FakeTimeout : public Timeout {
+public:
+    FakeTimeout() : Timeout(std::chrono::milliseconds{0}) {}
+
+    static auto TimedOut() -> bool { return false; }
+};
+
+} // namespace tt
