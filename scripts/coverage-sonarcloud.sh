c36daa5224f003619c93307061058b17102f526f
Danilo Spinella <oss@danyspin97.org>
2020-10-19 16:28:09 +0200

    Fix coverage for SonarCloud
    Add a shared library containing all the cli sources.

--- scripts/coverage-sonarcloud.sh
+++ scripts/coverage-sonarcloud.sh
@@ -6,7 +6,8 @@ mkdir "${GCOV_DIR}"
 cd "${MESON_BUILD_ROOT}"
 # -r consider only relative paths
 # -s remove .. prefix (we are running gcov from build folder)
-gcov-9 -p -s '..' -r src/*/*.g* src/*/*/*.g* test/*/*.g
+gcov-9 -p -s '..' -r src/*/*.g* src/*/*/*.g* test/*/*.g* \
+                     test/*/..*.g*
 # Remove coverage files for ext folder
 rm ext*.gcov
 mv *.gcov "${GCOV_DIR}"
