60a6f307dc587c13f4d7ec64c10d50027e289d83
Danilo Spinella <oss@danyspin97.org>
2020-10-19 14:30:38 +0200

    coverage-sonarcloud: Fix coverage file paths

--- scripts/coverage-sonarcloud.sh
+++ scripts/coverage-sonarcloud.sh
@@ -6,7 +6,7 @@ mkdir "${GCOV_DIR}"
 cd "${MESON_BUILD_ROOT}"
 # -r consider only relative paths
 # -s remove .. prefix (we are running gcov from build folder)
-gcov-9 -p -s '..' -r build/src/*/*.g* build/src/*/*/*.g* build/test/*/*.g
+gcov-9 -p -s '..' -r src/*/*.g* src/*/*/*.g* test/*/*.g
 # Remove coverage files for ext folder
 rm ext*.gcov
 mv *.gcov "${GCOV_DIR}"
