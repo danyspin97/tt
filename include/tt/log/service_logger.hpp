db8b155d9f50a3d677abb6f2441658785e5a708a
Danilo Spinella <oss@danyspin97.org>
2020-10-21 15:18:43 +0200

    ServiceLogger: Add new wrapper class around spdlog
    ServiceLogger init a logger and returns an newly
    created instance of ScriptLogger to use in Script constructor.
    
    Devired class must implement logging messages.

---
+++ include/tt/log/service_logger.hpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <oss@danyspin97.org>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <string>
+
+#include "tt/log/logger.hpp"
+
+namespace tt {
+class ScriptLogger;
+
+class ServiceLogger {
+public:
+    explicit ServiceLogger(std::string service_name);
+    virtual ~ServiceLogger();
+
+    // Returns a new instance of ScriptLogger
+    [[nodiscard]] auto GetScriptLogger() const -> ScriptLogger;
+
+    // Log messages to logger_
+    virtual void Start() const = 0;
+    virtual void Success() const = 0;
+    virtual void Fail() const = 0;
+
+protected:
+    [[nodiscard]] auto logger() const -> Logger;
+    [[nodiscard]] auto service_name() const -> const std::string &;
+
+private:
+    std::string service_name_;
+    Logger logger_;
+};
+
+} // namespace tt
---
+++ src/log/service_logger.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <oss@danyspin97.org>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tt/log/service_logger.hpp"
+
+#include <filesystem>
+
+#include "spdlog/async.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/spdlog.h"
+
+#include "tt/dirs.hpp"
+#include "tt/log/script_logger.hpp"
+#include "tt/status.hpp"
+
+tt::ServiceLogger::ServiceLogger(std::string service_name)
+    : service_name_(std::move(service_name)) {
+    // Check if the logger has already been added
+    if ((logger_ = spdlog::get(service_name_))) {
+        return;
+    }
+
+    std::filesystem::path logdir = Status::GetInstance().dirs().logdir();
+    logger_ = spdlog::basic_logger_mt<spdlog::async_factory>(
+        service_name_,
+        logdir / std::filesystem::path{service_name_ + std::string{".log"}});
+
+    logger_->set_pattern("{%d:%m:%Y %T} %v");
+}
+
+auto tt::ServiceLogger::GetScriptLogger() const -> ScriptLogger {
+    return ScriptLogger(logger_);
+}
+
+auto tt::ServiceLogger::logger() const -> Logger { return logger_; }
+
+auto tt::ServiceLogger::service_name() const -> const std::string & {
+    return service_name_;
+}
