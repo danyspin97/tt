9e3ed5a780ee9a44d7213ba168bd5465296a6c31
Danilo Spinella <oss@danyspin97.org>
2020-10-24 18:10:45 +0200

    Dirs: Transform into an interface

---
+++ include/tt/path/dirs.hpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <oss@danyspin97.org.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <filesystem>
+#include <string>
+
+namespace tt {
+
+class Dirs {
+public:
+    virtual ~Dirs() = default;
+
+    [[nodiscard]] virtual auto bindir() const -> std::filesystem::path = 0;
+    [[nodiscard]] virtual auto confdir() const -> std::filesystem::path = 0;
+    [[nodiscard]] virtual auto execline_prefix() const
+        -> std::filesystem::path = 0;
+    [[nodiscard]] virtual auto libexecdir() const -> std::filesystem::path = 0;
+    [[nodiscard]] virtual auto livedir() const -> std::filesystem::path = 0;
+    [[nodiscard]] virtual auto logdir() const -> std::filesystem::path = 0;
+    [[nodiscard]] virtual auto servicedir() const -> std::filesystem::path = 0;
+    [[nodiscard]] virtual auto statedir() const -> std::filesystem::path = 0;
+};
+
+} // namespace tt
