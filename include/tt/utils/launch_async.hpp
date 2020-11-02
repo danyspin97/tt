9852b062b4449e14ab52b4795f3c2cb1b9269314
Danilo Spinella <oss@danyspin97.org>
2020-11-02 22:07:03 +0100

    Add LaunchAsync util function

---
+++ include/tt/utils/launch_async.hpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <oss@danyspin97.org>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <future>
+
+namespace tt {
+
+template <class F> void LaunchAsync(F &&fun) {
+    auto futptr = std::make_shared<std::future<void>>();
+    *futptr = std::async(std::launch::async, [futptr, fun]() { fun(); });
+}
+
+} // namespace tt
