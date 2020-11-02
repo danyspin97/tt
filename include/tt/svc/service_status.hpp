3b4ec592c6bca2b4e66022d8debb9dc1cfe4108e
Danilo Spinella <oss@danyspin97.org>
2020-11-02 15:17:17 +0100

    Add ServiceStatus enum

---
+++ include/tt/svc/service_status.hpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020 Danilo Spinella <oss@danyspin97.org>.
+ *
+ * This file is part of tt
+ * (see https://github.com/danyspin97/tt).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+namespace tt {
+
+enum class ServiceStatus {
+    Reset = 0,
+    Starting = 1,
+    Up = 2,
+    Stopping = 3,
+    Down = 4
+};
+
+} // namespace tt
