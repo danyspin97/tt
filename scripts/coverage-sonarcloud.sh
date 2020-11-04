05f2bf9ec98aad19f2480a9efad301ba8237e1af
Danilo Spinella <oss@danyspin97.org>
2020-11-04 10:43:06 +0100

    coverage-sonarcloud: Do not consider gcov output

--- scripts/coverage-sonarcloud.sh
+++ scripts/coverage-sonarcloud.sh
@@ -7,7 +7,7 @@ cd "${MESON_BUILD_ROOT}"
 # -r consider only relative paths
 # -s remove .. prefix (we are running gcov from build folder)
 gcov-9 -p -s '..' -r src/*/*.g* src/*/*/*.g* test/*/*.g* \
-                     test/*/..*.g*
+                     test/*/..*.g* > /dev/null 2>&1
 # Remove coverage files for ext folder
 rm ext*.gcov
 mv *.gcov "${GCOV_DIR}"
