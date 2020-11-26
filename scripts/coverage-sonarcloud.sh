e4cb8e28d7c3e7842bf1f59d00212cc4d13343e7
Danilo Spinella <oss@danyspin97.org>
2020-11-26 15:07:40 +0100

    coverage-sonarcloud.sh: Use gcov-10 to generate coverage

--- scripts/coverage-sonarcloud.sh
+++ scripts/coverage-sonarcloud.sh
@@ -6,7 +6,7 @@ mkdir "${GCOV_DIR}"
 cd "${MESON_BUILD_ROOT}"
 # -r consider only relative paths
 # -s remove .. prefix (we are running gcov from build folder)
-gcov-9 -p -s '..' -r src/*/*.g* src/*/*/*.g* test/*/*.g* \
+gcov-10 -p -s '..' -r src/*/*.g* src/*/*/*.g* test/*/*.g* \
                      test/*/..*.g* > /dev/null 2>&1
 # Remove coverage files for ext folder
 rm ext*.gcov
